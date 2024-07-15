ce790056cf23e9d7e78b0fcc9319c42fc29d6c85
Mark Zhou <github.chaps279@passmail.net>
2024-07-15 16:39:23 -0400

    Update string_hash.h

--- strings/string_hash.h
+++ strings/string_hash.h
@@ -1,24 +1,24 @@
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const ll mod = (1ll << 61) - 1;
-const ll base = uniform_int_distribution<ll>(999, mod - 999)(rng);
+const ll MOD = (1ll << 61) - 1;
+const ll B = uniform_int_distribution<ll>(999, MOD - 999)(rng);
 
-vl pwb = {1};
+vl pow_b = {1};
 
-struct str_hash {
+struct StringHash {
     vl h;
 
-    str_hash(string s = "") {
+    StringHash(string s = "") {
         h.resize(sz(s) + 1);
         rep(i, sz(s)) {
-            h[i + 1] = (i128(h[i]) * base + s[i]) % mod;
+            h[i + 1] = (i128(h[i]) * B + s[i]) % MOD;
         }
-        while (sz(pwb) - 1 < sz(s)) {
-            pwb.pb(i128(pwb.back()) * base % mod);
+        while (sz(pow_b) - 1 < sz(s)) {
+            pow_b.pb(i128(pow_b.back()) * B % MOD);
         }
     }
 
     ll get(int l, int r) {
-        return ((h[r] - i128(h[l]) * pwb[r - l]) % mod + mod) % mod;
+        return ((h[r] - i128(h[l]) * pow_b[r - l]) % MOD + MOD) % MOD;
     }
 };
