37e4c4ec18afd6a600223ac064bd1bb47d410ac6
Mark Zhou <github.chaps279@passmail.net>
2024-07-15 16:36:08 -0400

    Create dsu.h

---
+++ datastructures/dsu.h
@@ -0,0 +1,27 @@
+struct DSU {
+    vi a;
+
+    DSU(int n) : a(n, -1) {}
+
+    int find(int x) {
+        return a[x] < 0 ? x : a[x] = find(a[x]);
+    }
+
+    int size(int x) {
+        return -a[find(x)];
+    }
+
+    bool join(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return 0;
+        }
+        if (a[x] > a[y]) {
+            swap(x, y);
+        }
+        a[x] += a[y];
+        a[y] = x;
+        return 1;
+    }
+};
