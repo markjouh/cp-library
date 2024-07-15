8fe0456287375ace0f250033f87dd509fd4df59e
Mark Zhou <zhoux25@georgeschool.org>
2024-07-15 16:35:43 -0400

    Update kruskals.h

--- graphs/kruskals.h
+++ graphs/kruskals.h
@@ -1,9 +1,13 @@
 vi kruskals(int n, vec<arr<int, 3>> &e) {
-    sort(all(e), [&](arr<int, 3> a, arr<int, 3> b) { return a[2] < b[2]; });
-    dsu d(n);
+    sort(all(e), [&](arr<int, 3> a, arr<int, 3> b) {
+        return a[2] < b[2];
+    });
+    DSU dsu(n);
     vi res;
     rep(i, sz(e)) {
-        if (d.unite(e[i][0], e[i][1])) res.pb(i);
+        if (dsu.join(e[i][0], e[i][1])) {
+            res.pb(i);
+        }
     }
     return res;
 }
