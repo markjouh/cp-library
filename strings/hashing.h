4bec2be289f839a1543560840178ee86402ff152
Mark Zhou <github.chaps279@passmail.net>
2024-07-17 16:47:48 -0400

    Update hashing.h

--- strings/hashing.h
+++ strings/hashing.h
@@ -1,11 +1,3 @@
-// https://rng-58.blogspot.com/2017/02/hashing-and-probability-of-collision.html
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-const ll mod = (1ll << 61) - 1;
-const ll base = uniform_int_distribution<ll>(1e3, mod - 1e3)(rng);
-
-vector<ll> pow_b = {1};
-
 struct Hash {
     ll val;
     int len;
@@ -22,23 +14,25 @@ struct Hash {
 };
 
 struct StringHash {
+    int len;
     vector<ll> hash;
 
-    StringHash(string s) {
-        hash.resize(sz(s) + 1);
-        for (int i = 0; i < sz(s); i++) {
+    StringHash() {}
+
+    StringHash(string s) : len(sz(s)), hash(len + 1) {
+        rep(i, len) {
             hash[i + 1] = (i128(hash[i]) * base + s[i]) % mod;
         }
-        while (sz(pow_b) < sz(hash)) {
+        while (sz(pow_b) <= len) {
             pow_b.push_back(i128(pow_b.back()) * base % mod);
         }
     }
 
-    Hash get_hash(int l, int r) {
+    Hash get(int l, int r) {
         return Hash(((hash[r] - i128(hash[l]) * pow_b[r - l]) % mod + mod) % mod, r - l);
     }
 
-    Hash get_hash() {
-        return get_hash(0, sz(hash) - 1);
+    Hash get() {
+        return get(0, sz(hash) - 1);
     }
 };
