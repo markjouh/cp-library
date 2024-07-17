f1734c2186a980c2bdc5dba8dcd35fdec48cd7a2
Mark Zhou <github.chaps279@passmail.net>
2024-07-17 14:26:43 -0400

    Create binary_indexed_tree_range_update.h

---
+++ datastructures/binary_indexed_tree_range_update.h
@@ -0,0 +1,35 @@
+struct BIT {
+    int n;
+    vi bit;
+
+    BIT(int x) : n(x), bit(n + 1) {}
+
+    BIT(vi &a) : n(sz(a)), bit(n + 1) {
+        rep(i, 1, n + 1) {
+            bit[i] += bit[i - 1];
+            if (i + (i & (-i)) <= n) {
+                bit[i + (i & (-i))] += bit[i];
+            }
+        }
+    }
+
+    void update(int r, int v) {
+        while (r) {
+            bit[r] += v;
+            r -= r & (-r);
+        }
+    }
+
+    void update(int l, int r, int v) {
+        update(r, v);
+        update(l, -v);
+    }
+
+    int query(int p) {
+        int res = 0;
+        for (p++; p <= n; p += p & (-p)) {
+            res += bit[p];
+        }
+        return res;
+    }
+};
