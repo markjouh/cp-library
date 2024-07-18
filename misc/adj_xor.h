ffe021f00ab8e50ee5623c0aed7ebfc4907fe194
Mark Zhou <github.chaps279@passmail.net>
2024-07-18 15:51:01 -0400

    Update adj_xor.h

--- misc/adj_xor.h
+++ misc/adj_xor.h
@@ -1,41 +1,41 @@
 // Add/delete elements, query for min pairwise XOR in set.
 
-struct adj_xor {
-    multiset<pair<int, int>> vals;
+struct AdjXor {
+    multiset<pi> vals;
     map<int, int> xors;
  
-    void add(pair<int, int> v) {
+    void add(pi v) {
         vals.insert(v);
         auto it = vals.find(v);
-        if (it != vals.begin()) {
-            xors[v.first ^ (*prev(it)).first]++;
+        if (it != begin(vals)) {
+            xors[v.fi ^ (*prev(it)).fi]++;
         }
-        if (it != prev(vals.end())) {
-            xors[v.first ^ (*next(it)).first]++;
+        if (it != prev(end(vals))) {
+            xors[v.fi ^ (*next(it)).fi]++;
         }
-        if (it != vals.begin() && it != prev(vals.end())) {
-            xors[(*prev(it)).first ^ (*next(it)).first]--;
+        if (it != begin(vals) && it != prev(end(vals))) {
+            xors[(*prev(it)).fi ^ (*next(it)).fi]--;
         }
     }
  
-    void remove(pair<int, int> v) {
+    void remove(pi v) {
         auto it = vals.find(v);
-        if (it != vals.begin()) {
-            xors[v.first ^ (*prev(it)).first]--;
+        if (it != begin(vals)) {
+            xors[v.fi ^ (*prev(it)).fi]--;
         }
-        if (it != prev(vals.end())) {
-            xors[v.first ^ (*next(it)).first]--;
+        if (it != prev(end(vals))) {
+            xors[v.fi ^ (*next(it)).fi]--;
         }
-        if (it != vals.begin() && it != prev(vals.end())) {
-            xors[(*prev(it)).first ^ (*next(it)).first]++;
+        if (it != begin(vals) && it != prev(end(vals))) {
+            xors[(*prev(it)).fi ^ (*next(it)).fi]++;
         }
         vals.erase(it);
     }
  
     int get_min() {
-        while (!xors.empty() && xors.begin()->second == 0) {
-            xors.erase(xors.begin());
+        while (sz(xors) && begin(xors)->se == 0) {
+            xors.erase(begin(xors));
         }
-        return xors.empty() ? INT_MAX : xors.begin()->first;
+        return xors.empty() ? INT_MAX : begin(xors)->fi;
     }
 };
