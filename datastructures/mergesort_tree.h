b7e0d1836b15c9e75075303999a30d49b3df0718
Mark Zhou <github.chaps279@passmail.net>
2024-07-24 21:14:14 -0400

    Create mergesort_tree.h

---
+++ datastructures/mergesort_tree.h
@@ -0,0 +1,31 @@
+struct MergeTree {
+    int n;
+    vec<iset<int>> mt;
+
+    MergeTree(int x) : n(x), mt(n + 1) {}
+
+    void add(int p, int v) {
+        for (p++; p <= n; p += p & (-p)) {
+            mt[p].ins(v);
+        }
+    }
+
+    void rem(int p, int v) {
+        for (p++; p <= n; p += p & (-p)) {
+            mt[p].era(v);
+        }
+    }
+
+    int query(int r, int v) {
+        int res = 0;
+        while (r) {
+            res += mt[r].ook(v);
+            r -= r & (-r);
+        }
+        return res;
+    }
+
+    int query(int l, int r, int v) {
+        return query(r, v) - query(l, v);
+    }
+};
