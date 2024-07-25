fd58b32a9c7107e0b5a932a393609059b6c4462a
Mark Zhou <github.chaps279@passmail.net>
2024-07-25 17:51:03 -0400

    Update mergesort_tree.h

--- datastructures/mergesort_tree.h
+++ datastructures/mergesort_tree.h
@@ -5,27 +5,26 @@ struct MergeTree {
     MergeTree(int x) : n(x), mt(n + 1) {}
 
     void add(int p, int v) {
-        for (p++; p <= n; p += p & (-p)) {
+        for (p++; p <= n; p += p & -p) {
             mt[p].ins(v);
         }
     }
 
     void rem(int p, int v) {
-        for (p++; p <= n; p += p & (-p)) {
+        for (p++; p <= n; p += p & -p) {
             mt[p].era(v);
         }
     }
 
     int query(int r, int v) {
         int res = 0;
-        while (r) {
-            res += mt[r].ook(v);
-            r -= r & (-r);
+        for (r++; r > 0; r -= r & -r) {
+            res += mt[r].order_of_key(v);
         }
         return res;
     }
 
     int query(int l, int r, int v) {
-        return query(r, v) - query(l, v);
+        return query(r, v) - query(l - 1, v);
     }
 };
