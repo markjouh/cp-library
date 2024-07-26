7176b529721e8f73503fa205b0cf6bb84ee87eb4
Mark Zhou <github.chaps279@passmail.net>
2024-07-26 16:42:46 -0400

    Update mod_int.h

--- math/mod_int.h
+++ math/mod_int.h
@@ -1,34 +1,68 @@
-template<int M> struct Mint {
+template<int MOD> struct Mint {
     int v;
-    
-    Mint() {}
-
-    Mint(ll x) {
-        v = int(-M < x && x < M ? x : x % M);
-        if (v < 0) v += M;
+ 
+    Mint(ll x = 0) {
+        v = int(-MOD < x && x < MOD ? x : x % MOD);
+        if (v < 0) {
+            v += MOD;
+        }
     }
  
     friend Mint pow(Mint base, int exp) {
         Mint res = 1;
         while (exp) {
-            if (exp & 1) res *= base;
+            if (exp & 1) {
+                res *= base;
+            }
             base *= base;
             exp >>= 1;
         }
         return res;
     }
-
-    Mint &operator+=(Mint b) { if ((v += b.v) >= M) v -= M; return *this; }
-    Mint &operator-=(Mint b) { if ((v -= b.v) < 0) v += M; return *this; }
-    Mint &operator*=(Mint b) { v = int(ll(v) * b.v % M); return *this; }
-    Mint &operator/=(Mint b) { v = int(ll(v) * pow(b, M - 2).v % M); return *this; }
-
-    friend Mint operator+(Mint a, Mint b) { return a += b; }
-    friend Mint operator-(Mint a, Mint b) { return a -= b; }
-    friend Mint operator*(Mint a, Mint b) { return a *= b; }
-    friend Mint operator/(Mint a, Mint b) { return a /= b; }
-
-    friend ostream &operator<<(ostream &os, Mint a) { return os << a.v; }
+ 
+    Mint &operator+=(Mint b) {
+        if ((v += b.v) >= MOD) {
+            v -= MOD;
+        }
+        return *this;
+    }
+ 
+    Mint &operator-=(Mint b) {
+        if ((v -= b.v) < 0) {
+            v += MOD;
+        }
+        return *this;
+    }
+ 
+    Mint &operator*=(Mint b) {
+        v = int(1ll * v * b.v % MOD);
+        return *this;
+    }
+ 
+    Mint &operator/=(Mint b) {
+        v = int(1ll * v * pow(b, MOD - 2).v % MOD);
+        return *this;
+    }
+ 
+    friend Mint operator+(Mint a, Mint b) {
+        return a += b;
+    }
+ 
+    friend Mint operator-(Mint a, Mint b) {
+        return a -= b;
+    }
+ 
+    friend Mint operator*(Mint a, Mint b) {
+        return a *= b;
+    }
+ 
+    friend Mint operator/(Mint a, Mint b) {
+        return a /= b;
+    }
+ 
+    friend ostream &operator<<(ostream &os, Mint a) {
+        return os << a.v;
+    }
 };
 
 // using mi = Mint<int(1e9 + 7)>;
