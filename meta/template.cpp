f90260b26527e31b7f9b116da32eca22f2496399
Mark Zhou <github.chaps279@passmail.net>
2024-07-28 18:26:13 -0400

    Update template.cpp

--- meta/template.cpp
+++ meta/template.cpp
@@ -19,9 +19,7 @@ using namespace std;
 #define per(...) arg4(__VA_ARGS__, per3, per2) (__VA_ARGS__)
 
 #define bg(x) begin(x)
-#define ed(x) end(x)
-#define all(x) begin(x), end(x)
-#define rall(x) rbegin(x), rend(x)
+#define all(x) bg(x), end(x)
 #define sz(x) int(size(x))
 
 #define acc accumulate
@@ -58,54 +56,32 @@ using vs = vec<str>;
 using vpi = vec<pi>;
 using vpl = vec<pl>;
 
-using vvi = vec<vi>;
-using vvl = vec<vl>;
-using vvpi = vec<vpi>;
-using vvpl = vec<vpl>;
-
 template<class T> bool ckmin(T &a, T b) {
-    return b < a ? a = b, 1 : 0;
+  return b < a ? a = b, 1 : 0;
 }
 
 template<class T> bool ckmax(T &a, T b) {
-    return b > a ? a = b, 1 : 0;
+  return b > a ? a = b, 1 : 0;
 }
 
 template<class T, class U> T bsmin(T lo, T hi, U f) {
-    assert(lo < ++hi);
-    for (T i = 1ll << __lg(hi - lo); i; i >>= 1) {
-        if (hi - i >= lo && f(hi - i)) {
-            hi -= i;
-        }
+  hi++;
+  for (T i = 1ll << __lg(hi - lo); i; i >>= 1) {
+    if (hi - i >= lo && f(hi - i)) {
+      hi -= i;
     }
-    return hi;
+  }
+  return hi;
 }
 
 template<class T, class U> T bsmax(T lo, T hi, U f) {
-    assert(--lo < hi);
-    for (T i = 1ll << __lg(hi - lo); i; i >>= 1) {
-        if (lo + i <= hi && f(lo + i)) {
-            lo += i;
-        }
+  lo--;
+  for (T i = 1ll << __lg(hi - lo); i; i >>= 1) {
+    if (lo + i <= hi && f(lo + i)) {
+      lo += i;
     }
-    return lo;
-}
-
-template<class T> void remdup(vec<T> &a) {
-    sort(all(a));
-    a.resize(unique(all(a)) - bg(a));
-}
-
-void set_io(str s = "") {
-    cin.tie(0)->sync_with_stdio(0);
-    cin.exceptions(ios::badbit | ios::failbit);
-    cout << setprecision(12) << fixed;
-#ifndef LOCAL
-    if (sz(s)) {
-        freopen((s + ".in").c_str(), "r", stdin);
-        freopen((s + ".out").c_str(), "w", stdout);
-    }
-#endif
+  }
+  return lo;
 }
 
 const int inf = INT_MAX / 2;
@@ -115,10 +91,12 @@ void solve() {
 }
 
 int main() {
-    set_io();
-    int t = 1;
-    // cin >> t;
-    while (t--) {
-        solve();
-    }
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
 }
