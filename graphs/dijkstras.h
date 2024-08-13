b5ea6ef2cf8c1c84be302b5bcdf10a54f33225b2
Mark Zhou <github.chaps279@passmail.net>
2024-08-13 18:04:15 -0400

    Update dijkstras.h

--- graphs/dijkstras.h
+++ graphs/dijkstras.h
@@ -1,7 +1,7 @@
-pair<vl, vi> dijkstras(vec<vpi> &g, int r = 0) {
-    vl dist(sz(g), infl);
+template<class T> pair<vec<T>, vi> dijkstras(vec<vpi> &g, int r = 0) {
+    vec<T> dist(sz(g), numeric_limits<T>::max());
     vi from(sz(g), -1);
-    priority_queue<pl, vec<pl>, greater<pl>> pq;
+    priority_queue<pair<T, int>, vec<pair<T, int>>, greater<pair<T, int>>> pq;
     dist[r] = 0;
     pq.emplace(0, r);
     while (sz(pq)) {
