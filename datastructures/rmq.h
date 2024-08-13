8de651f67720e31972473df386544a707a0111b2
Mark Zhou <github.chaps279@passmail.net>
2024-08-13 12:41:34 -0400

    Update rmq.h

--- datastructures/rmq.h
+++ datastructures/rmq.h
@@ -1,19 +1,28 @@
-struct RMQ {
+template<class T, bool use_max = false> struct RMQ {
     int n, lg;
-    vec<vi> rmq;
+    vec<vec<T>> rmq;
 
-    RMQ(vi &a) : n(sz(a)), lg(__lg(n) + 1), rmq(lg) {
+    T merge(T a, T b) {
+        if constexpr (use_max) {
+            return max(a, b);
+        } else {
+            return min(a, b);
+        }
+    }
+
+    RMQ(vec<T> &a) : n(sz(a)), lg(__lg(n) + 1), rmq(lg) {
         rmq[0] = a;
         rep(i, 1, lg) {
             rmq[i].resize(n - (1 << i) + 1);
             rep(j, sz(rmq[i])) {
-                rmq[i][j] = min(rmq[i - 1][j], rmq[i - 1][j + (1 << (i - 1))]);
+                rmq[i][j] = merge(rmq[i - 1][j], rmq[i - 1][j + (1 << (i - 1))]);
             }
         }
     }
 
-    int query(int l, int r) {
+    T query(int l, int r) {
+        assert(l <= r);
         int i = __lg(++r - l);
-        return min(rmq[i][l], rmq[i][r - (1 << i)]);
+        return merge(rmq[i][l], rmq[i][r - (1 << i)]);
     }
 };
