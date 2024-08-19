b859b0d6837f04b82d5f2a7b2f6f311b4a626b12
Mark Zhou <github.chaps279@passmail.net>
2024-08-18 21:40:07 -0400

    Update hashing.h

--- strings/hashing.h
+++ strings/hashing.h
@@ -13,7 +13,7 @@ struct Hash {
     Hash(ll x, int y) : val(x), len(y) {}
 
     Hash operator+(Hash b) {
-        return Hash((i128(val) * pow_b[b.len] + b.val) % mod, len + b.len);
+        return Hash((__int128_t(val) * pow_b[b.len] + b.val) % mod, len + b.len);
     }
 
     bool operator==(Hash b) {
@@ -27,15 +27,15 @@ struct StringHash {
 
     StringHash(string s) : len(sz(s)), hash(len + 1) {
         rep(i, len) {
-            hash[i + 1] = (i128(hash[i]) * base + s[i]) % mod;
+            hash[i + 1] = (__int128_t(hash[i]) * base + s[i]) % mod;
         }
         while (sz(pow_b) <= len) {
-            pow_b.push_back(i128(pow_b.back()) * base % mod);
+            pow_b.push_back(__int128_t(pow_b.back()) * base % mod);
         }
     }
 
     Hash get(int l, int r) {
-        return Hash(((hash[r] - i128(hash[l]) * pow_b[r - l]) % mod + mod) % mod, r - l);
+        return Hash(((hash[r] - __int128_t(hash[l]) * pow_b[r - l]) % mod + mod) % mod, r - l);
     }
 
     Hash get() {
