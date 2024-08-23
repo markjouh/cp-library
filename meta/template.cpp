8de85b19bddaa63081445cd6397fcc8f1b296f68
Mark Zhou <github.chaps279@passmail.net>
2024-08-22 23:44:26 -0400

    Update template.cpp

--- meta/template.cpp
+++ meta/template.cpp
@@ -1,42 +1,12 @@
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <cmath>
-#include <cstdint>
-#include <cstring>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+#define all(x) begin(x), end(x)
+#define sz(x) int(size(x))
+
 using ll = long long;
 using ld = long double;
-
-template<class T> constexpr int sz(T x) {
-    return static_cast<int>((x).size());
-}
-
-template<class T> constexpr int lg2(T x) {
-    assert(x > 0);
-#ifdef __GNUC__
-    return static_cast<int>(__lg2(x));
-#else
-    int res = 0;
-    while (x >>= 1) {
-        res++;
-    }
-    return res;
-#endif
-}
 
 template<class T> bool ckmin(T &a, const T b) {
     return b < a ? a = b, 1 : 0;
@@ -49,7 +19,7 @@ template<class T> bool ckmax(T &a, const T b) {
 template<class T, class U> T bsmin(T lo, T hi, U f) {
     assert(lo <= hi);
     hi++;
-    for (T i = T(1) << lg2(hi - lo); i > 0; i >>= 1) {
+    for (T i = T(1) << __lg(hi - lo); i > 0; i >>= 1) {
         hi -= (hi - i >= lo && f(hi - i)) * i;
     }
     return hi;
@@ -58,7 +28,7 @@ template<class T, class U> T bsmin(T lo, T hi, U f) {
 template<class T, class U> T bsmax(T lo, T hi, U f) {
     assert(lo <= hi);
     lo--;
-    for (T i = T(1) << lg2(hi - lo); i > 0; i >>= 1) {
+    for (T i = T(1) << __lg(hi - lo); i > 0; i >>= 1) {
         lo += (lo + i <= hi && f(lo + i)) * i;
     }
     return lo;
