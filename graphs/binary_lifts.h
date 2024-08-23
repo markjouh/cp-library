815536dc38d24c151b393b6c5c5be4229556e136
Mark Zhou <github.chaps279@passmail.net>
2024-08-23 00:47:47 -0400

    Update and rename lca.h to binary_lifts.h

--- graphs/binary_lifts.h
+++ graphs/binary_lifts.h
@@ -1,20 +1,24 @@
-struct LCA {
-    int n, lg;
-    vi dep;
-    vec<vi> up;
+/* Given a rooted tree, computes binary lifts, which enable O(log(N)) LCA.
+ * TODO: arbitrary path aggregates?
+ */
 
-    LCA(vec<vi> &g) : n(sz(g)), lg(__lg(n) + 1), dep(n, inf), up(n, vi(lg, -1)) {
+struct binary_lifts {
+    int n, lg;
+    vector<int> dep;
+    vector<vector<int>> up;
+
+    binary_lifts(const vector<vector<int>> &g, int root = 0) : n(sz(g)), lg(__lg(n) + 1), dep(n, -1), up(n, vector<int>(lg, -1)) {
         queue<int> q;
-        dep[0] = 0;
-        q.push(0);
+        dep[root] = 0;
+        q.push(root);
         while (sz(q)) {
             int u = q.front();
             q.pop();
             for (int v : g[u]) {
-                if (dep[v] == inf) {
+                if (dep[v] == -1) {
                     dep[v] = dep[u] + 1;
                     up[v][0] = u;
-                    rep(i, 1, lg) {
+                    for (int i = 1; i < lg; i++) {
                         if (up[v][i - 1] == -1) {
                             break;
                         }
@@ -26,8 +30,12 @@ struct LCA {
         }
     }
 
+    int par(int x) {
+        return up[x][0];
+    }
+
     int anc(int x, int k) {
-        rep(i, lg) {
+        for (int i = 0; i < lg; i++) {
             if (x != -1 && ((k >> i) & 1)) {
                 x = up[x][i];
             }
@@ -43,7 +51,7 @@ struct LCA {
         if (x == y) {
             return x;
         }
-        per(i, lg) {
+        for (int i = lg - 1; i >= 0; i--) {
             if (up[x][i] != up[y][i]) {
                 x = up[x][i];
                 y = up[y][i];
