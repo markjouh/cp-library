4bce36ca04e0a7d893b32ba4f27d74663f67efcc
Mark Zhou <github.chaps279@passmail.net>
2024-08-23 01:13:20 -0400

    Create binary_search.h

---
+++ misc/binary_search.h
@@ -0,0 +1,20 @@
+/* Nah, I'd learn useless algorithms
+ */
+
+template<class T, class U> T bsmin(T lo, T hi, U f) {
+    assert(lo <= hi);
+    hi++;
+    for (T i = T(1) << __lg(hi - lo); i > 0; i >>= 1) {
+        hi -= (hi - i >= lo && f(hi - i)) * i;
+    }
+    return hi;
+}
+
+template<class T, class U> T bsmax(T lo, T hi, U f) {
+    assert(lo <= hi);
+    lo--;
+    for (T i = T(1) << __lg(hi - lo); i > 0; i >>= 1) {
+        lo += (lo + i <= hi && f(lo + i)) * i;
+    }
+    return lo;
+}
