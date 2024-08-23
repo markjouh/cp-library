5a5e0959f6b55e4cb4884b75f1c74010c73bff49
Mark Zhou <github.chaps279@passmail.net>
2024-08-22 23:04:44 -0400

    Update fenwick_tree_extended.h

--- datastructures/fenwick_tree_extended.h
+++ datastructures/fenwick_tree_extended.h
@@ -1,17 +1,17 @@
-/* A data structure that uses two fenwick trees in conjunction to support both
- * range adds and range updates in log(N).
+/* A data structure that uses two fenwick trees in tandem to support both
+ * range adds and range queries in O(log(N)).
  * A much smaller and faster alternative to lazy segment trees for this particular
  * use case.
  */
 
 template<class T> struct extended_fenwick {
     int n;
-    fenwick_tree<T> in, out;
+    fenwick_tree<T> inside_pref, outside_pref;
 
-    extended_fenwick(int x) : n(x), in(n), out(n) {}
+    extended_fenwick(int x) : n(x), inside_pref(n), outside_pref(n) {}
 
     T sum(int r) {
-        return in.sum(r - 1) + out.sum(n - r - 1) * (r + 1);
+        return inside_pref.sum(r - 1) + outside_pref.sum(n - r - 1) * (r + 1);
     }
 
     T sum(int l, int r) {
@@ -19,8 +19,8 @@ template<class T> struct extended_fenwick {
     }
 
     void add(int r, T v) {
-        in.add(r, v * (r + 1));
-        out.add(n - r - 1, v);
+        inside_pref.add(r, v * (r + 1));
+        outside_pref.add(n - r - 1, v);
     }
 
     void add(int l, int r, T v) {
