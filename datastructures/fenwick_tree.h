01083cc0cc000c195f053998715b8bd738d6ede5
Mark Zhou <github.chaps279@passmail.net>
2024-08-22 21:34:51 -0400

    Update fenwick_tree.h

--- datastructures/fenwick_tree.h
+++ datastructures/fenwick_tree.h
@@ -1,33 +1,35 @@
-template<class T> struct FT {
+template<class T> struct fenwick_tree {
     int n;
-    vec<T> ft;
+    vector<T> tree;
 
-    FT(int x) : n(x), ft(n + 1) {}
+    fenwick_tree(int x) : n(x), tree(n + 1) {}
 
-    FT(vec<T> &a) : n(sz(a)), ft(n + 1) {
-        rep(i, 1, n + 1) {
-            ft[i] += a[i - 1];
+    fenwick_tree(const vector<T> &a) : n(sz(a)), tree(n + 1) {
+        for (int i = 1; i <= n; i++) {
+            tree[i] += a[i - 1];
             if (i + (i & -i) <= n) {
-                ft[i + (i & -i)] += ft[i];
+                tree[i + (i & -i)] += tree[i];
             }
         }
     }
 
+    void add(int p, T v) {
+        assert(p >= 0);
+        for (p++; p <= n; p += p & -p) {
+            tree[p] += v;
+        }
+    }
+
     T sum(int r) {
+        assert(r < n);
         T res = 0;
         for (r++; r > 0; r -= r & -r) {
-            res += ft[r];
+            res += tree[r];
         }
         return res;
     }
 
     T sum(int l, int r) {
         return sum(r) - sum(l - 1);
-    }
-
-    void add(int p, T v) {
-        for (p++; p <= n; p += p & -p) {
-            ft[p] += v;
-        }
     }
 };
