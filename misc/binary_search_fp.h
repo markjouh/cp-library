077d0d62e45edc77382e3444c20ec25507d51a61
Mark Zhou <github.chaps279@passmail.net>
2024-08-24 13:51:11 -0400

    Create binary_search_fp.h

---
+++ misc/binary_search_fp.h
@@ -0,0 +1,21 @@
+template<class T> db bsmax_fp(db lo, db hi, T f) {
+    assert(sizeof(uint64_t) >= sizeof(db));
+    assert(lo <= hi);
+    uint64_t l = reinterpret_cast<uint64_t&>(lo) - 1, r = reinterpret_cast<uint64_t&>(hi);
+    for (uint64_t i = 1ull << __lg(r - l); i > 0; i >>= 1) {
+        uint64_t nxt = l + i;
+        l += (nxt <= r && f(reinterpret_cast<db&>(nxt))) * i;
+    }
+    return reinterpret_cast<db&>(l);
+}
+
+template<class T> db bsmin_fp(db lo, db hi, T f) {
+    assert(sizeof(uint64_t) >= sizeof(db));
+    assert(lo <= hi);
+    uint64_t l = reinterpret_cast<uint64_t&>(lo), r = reinterpret_cast<uint64_t&>(hi) + 1;
+    for (uint64_t i = 1ull << __lg(r - l); i > 0; i >>= 1) {
+        uint64_t nxt = r - i;
+        r -= (nxt >= l && f(reinterpret_cast<db&>(nxt))) * i;
+    }
+    return reinterpret_cast<db&>(r);
+}
