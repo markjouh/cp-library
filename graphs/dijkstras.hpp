818a74284c02bb720691cf74cdd8df5f0893a995
Mark Zhou <github.chaps279@passmail.net>
2024-08-26 03:00:53 -0400

    Update dijkstras.hpp

--- graphs/dijkstras.hpp
+++ graphs/dijkstras.hpp
@@ -1,7 +1,7 @@
 /* Given a weighted graph, computes and constructs the shortest paths from the
  * starting vertex.
  * `dijkstras((V, E))`: O((|V| + |E|) * log(|V|))
- *
+ */
 
 template<class T> auto dijkstras(const vector<vector<pair<int, int>>> &g, int start = 0) {
     priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> pq;
