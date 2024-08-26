d0bbcfd94fbeadf3a80f79f540d1570a7684c198
Mark Zhou <github.chaps279@passmail.net>
2024-08-25 22:17:45 -0400

    Update tree_isomorphism.h

--- graphs/tree_isomorphism.h
+++ graphs/tree_isomorphism.h
@@ -1,29 +1,29 @@
-/* Takes in rooted trees and computes perfect hashes of all subtrees in O(N * log(N)).
+/* Takes in rooted trees and generates perfect hashes of all subtrees in O(N * log(N)).
  * Two trees are isomorphic if and only if their hashes match.
  */
 
 struct tree_isomorphism {
     map<vector<int>, int> hashes;
 
-    vector<int> compute(const vector<vector<int>> &g, int root = 0) {
+    vector<int> gen(const vector<vector<int>> &g, int root = 0) {
         vector<int> res(sz(g));
-
-        auto dfs = [&](int u, int par, auto &&self) -> void {
-            vector<int> children;
-            for (int v : g[u]) {
-                if (v != par) {
-                    self(v, u, self);
-                    children.push_back(res[v]);
-                }
-            }
-            sort(all(children));
-            if (!hashes.count(children)) {
-                hashes[children] = sz(hashes);
-            }
-            res[u] = hashes[children];
-        };
-
-        dfs(root, -1, dfs);
+        dfs(root, -1, g, res);
         return res;
+    }
+
+private:
+    void dfs(int u, int par, auto &g, auto &res) {
+        vector<int> children;
+        for (int v : g[u]) {
+            if (v != par) {
+                dfs(v, u, g, res);
+                children.push_back(res[v]);
+            }
+        }
+        sort(all(children));
+        if (!hashes.count(children)) {
+            hashes[children] = sz(hashes);
+        }
+        res[u] = hashes[children];
     }
 };
