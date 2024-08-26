bf2f5e26522cee15645133735212133e5c87544a
Mark Zhou <github.chaps279@passmail.net>
2024-08-26 02:56:37 -0400

    Update kosaraju_scc.hpp

--- graphs/kosaraju_scc.hpp
+++ graphs/kosaraju_scc.hpp
@@ -1,7 +1,7 @@
 /* Kosaraju's algorithm for SCCs.
- * comp[x] is the component id, numbered [0, sccs), of x.
- * c_adj represents the condensation graph through connections between component ids.
- * members[c] holds the original vertex numbers of the members of component c.
+ * `comp[x]` is the component id, numbered [0, sccs), of x.
+ * `c_adj` represents the condensation graph through connections between component ids.
+ * `members[c]` holds the original vertex numbers of the members of component c.
  * `kosaraju_scc(G)`: O(|G|)
  */
 
