ca3fb695edcb0cec1b729e634db78a7d51b9e2fa
Mark Zhou <github.chaps279@passmail.net>
2024-08-26 11:04:18 -0400

    Update segment_tree.hpp

--- datastructures/segment_tree.hpp
+++ datastructures/segment_tree.hpp
@@ -1,5 +1,6 @@
 /* A data structure that stores an array of objects with an identity element
  * and associative binary operator.
+ * `segment_tree(A)`: O(|A|)
  * `update(x, y)`, `query(x, y)`: O(log(N))
  */
 
