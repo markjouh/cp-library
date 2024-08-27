12d66ba9062488258a129b5dfda74d8b68617c22
Mark Zhou <xinyunzhouu@gmail.com>
2024-08-26 23:04:42 -0400

    Add depq library checker test

---
+++ verify/library_checker/data_structure/double_ended_priority_queue.test.cpp
@@ -0,0 +1,48 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/double_ended_priority_queue"
+
+#include "../../boilerplate.hpp"
+#include "../../../datastructures/w_ary_tree.hpp"
+
+const int lo = numeric_limits<int>::min(), hi = numeric_limits<int>::max();
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> queries, vals;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        queries.push_back(x);
+        vals.push_back(x);
+    }
+    for (int i = 0; i < q; i++) {
+        int t;
+        cin >> t;
+        if (t == 0) {
+            int x;
+            cin >> x;
+            queries.push_back(x);
+            vals.push_back(x);
+        } else if (t == 1) {
+            queries.push_back(lo);
+        } else {
+            queries.push_back(hi);
+        }
+    }
+    sort(all(vals));
+    vals.resize(unique(all(vals)) - begin(vals));
+    w_ary_tree<500000> ds;
+    for (int x : queries) {
+        if (x == lo) {
+            int rem = ds.get_min();
+            cout << vals[rem] << '\n';
+            ds.extract(rem);
+        } else if (x == hi) {
+            int rem = ds.get_max();
+            cout << vals[rem] << '\n';
+            ds.extract(rem);
+        } else {
+            ds.insert(lower_bound(all(vals), x) - begin(vals));
+        }
+    }
+}
