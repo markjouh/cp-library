99674c85d063c98deb3de7ea608b1061d783ae2d
Mark Zhou <xinyunzhouu@gmail.com>
2024-08-26 22:19:58 -0400

    Add hashing test

---
+++ verify/library_checker/string/enumerate_palindromes.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/number_of_substrings"
+
+#include "../../boilerplate.hpp"
+#include "../../../numeric/binary_search.hpp"
+#include "../../../strings/hashing.hpp"
+
+using namespace hashing;
+
+int main() {
+    string s;
+    cin >> s;
+    string rev = s;
+    reverse(all(rev));
+    str_hash a(s), b(rev);
+    const int n = sz(s);
+    vector<int> ans(2 * n - 1);
+    for (int i = 0; i < n; i++) {
+        ans[2 * i] = 2 * bsmax(0, min(i, n - i - 1), [&](int d) {
+            return a.get(i - d, i) == b.get(n - i - 1 - d, n - i - 1);
+        }) + 1;
+    }
+    for (int i = 0; i < n - 1; i++) {
+        ans[2 * i + 1] = 2 * bsmax(0, min(i, n - i - 2), [&](int d) {
+            return a.get(i - d, i) == b.get(n - i - 2 - d, n - i - 2);
+        });
+    }
+    for (int i = 0; i < 2 * n - 1; i++) {
+        cout << ans[i] << ' ';
+    }
+    cout << '\n';
+}
