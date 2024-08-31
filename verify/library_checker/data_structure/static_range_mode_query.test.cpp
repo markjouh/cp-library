31fc92946355b23b865feb2ff24805b3659f3518
Mark Zhou <xinyunzhouu@gmail.com>
2024-08-31 12:00:19 -0400

    Use hash table in mode test

--- verify/library_checker/data_structure/static_range_mode_query.test.cpp
+++ verify/library_checker/data_structure/static_range_mode_query.test.cpp
@@ -1,6 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/static_range_mode_query"
 
 #include "../../boilerplate.hpp"
+#include "../../../datastructures/hash_table.hpp"
 #include "../../../datastructures/mo_queries.hpp"
 
 int main() {
@@ -16,17 +17,17 @@ int main() {
     cin >> l >> r;
     mo.insert(l, r - 1);
   }
-  map<int, int> freq;
+  HashTable<int, int> freq;
   set<pair<int, int>> st;
   auto add = [&](int p) {
-    if (freq.count(a[p])) {
+    if (freq.find(a[p]) != freq.end()) {
       st.extract({freq[a[p]], a[p]});
     }
     freq[a[p]]++;
     st.insert({freq[a[p]], a[p]});
   };
   auto del = [&](int p) {
-    if (freq.count(a[p])) {
+    if (freq.find(a[p]) != freq.end()) {
       st.extract({freq[a[p]], a[p]});
     }
     freq[a[p]]--;
