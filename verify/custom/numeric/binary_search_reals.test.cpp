20812573b4e5c168e2d127a0fe533581521027c0
Mark Zhou <xinyunzhouu@gmail.com>
2024-08-31 22:17:54 -0400

    Add binary search on reals test

---
+++ verify/custom/numeric/binary_search_reals.test.cpp
@@ -0,0 +1,50 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "../../boilerplate.hpp"
+#include "../../../local/my_random.hpp"
+#include "../../../numeric/binary_search_reals.hpp"
+
+using namespace my_random;
+
+const int TESTS = 1e6;
+const double MXV = 1e12, ERROR = 1e-3;
+
+void test() {
+  // Search for exact double value, should have no error
+  const double t1 = randreal<double>(0, DBL_MAX);
+  double left = bsmax_reals(0, DBL_MAX, [&](double x) {
+    return x <= t1;
+  });
+  assert(left == t1);
+  double right = bsmin_reals(0, DBL_MAX, [&](double x) {
+    return x >= t1;
+  });
+  assert(right == t1);
+
+  // Try computing sqrt
+  const double t2 = randreal<double>(0, MXV);
+  double sqrt_val = bsmax_reals(0, MXV, [&](double x) {
+    return x * x <= t2;
+  });
+  double sqrt_res = sqrt_val * sqrt_val;
+  assert(sqrt_res <= t2 && t2 - sqrt_res < ERROR);
+
+  // Try computing cbrt
+  const double t3 = randreal<double>(0, MXV);
+  double cbrt_val = bsmax_reals(0, MXV, [&](double x) {
+    return x * x * x <= t3;
+  });
+  double cbrt_res = cbrt_val * cbrt_val * cbrt_val;
+  assert(cbrt_res <= t3 && t3 - cbrt_res < ERROR);
+}
+
+int main() {
+  for (int i = 0; i < TESTS; i++) {
+    test();
+  }
+  cerr << "Tests passed\n";
+
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << '\n';
+}
