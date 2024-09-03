d3f5979ad1564dc3c0e55910db140b6913949714
Mark Zhou <xinyunzhouu@gmail.com>
2024-09-03 11:43:32 -0400

    Edit random

--- utils/random.hpp
+++ utils/random.hpp
@@ -3,8 +3,18 @@
 mt19937_64 rng_64(chrono::steady_clock::now().time_since_epoch().count());
 
 template <class T>
+T randint(T n) {
+  return uniform_int_distribution<T>(0, n)(rng_64);
+}
+
+template <class T>
 T randint(T lo, T hi) {
   return uniform_int_distribution<T>(lo, hi)(rng_64);
+}
+
+template <class T>
+double randreal(T n = 1) {
+  return uniform_real_distribution<double>(0, n)(rng_64);
 }
 
 template <class T>
