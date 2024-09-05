31b1883abce03623dce5af3b4d786609fce3f196
Mark Zhou <xinyunzhouu@gmail.com>
2024-09-05 16:37:25 -0400

    Edits

--- graphs/euler_cycle.hpp
+++ graphs/euler_cycle.hpp
@@ -3,16 +3,16 @@
 // https://cp-algorithms.com/graph/euler_path.html
 // https://codeforces.com/contest/1994/submission/279728160
 
-vec<int> euler_cycle(const vec<vec<int>> &g) {
-  vec<vec<pii>> h(sz(g));
+vector<int> euler_cycle(const vector<vector<int>> &g) {
+  vector<vector<pair<int, int>>> h(sz(g));
   vector<int> deg(sz(g));
   int cnt = 0;
-  rep(u, sz(g)) {
+  for (int u = 0; u < sz(g); u++) {
     int self = 0;
     for (int v : g[u]) {
       if ((u == v && self % 2) || u < v) {
-        h[u].eb(v, cnt);
-        h[v].eb(u, cnt);
+        h[u].emplace_back(v, cnt);
+        h[v].emplace_back(u, cnt);
         deg[u]++, deg[v]++;
         cnt++;
       }
@@ -20,13 +20,13 @@ vec<int> euler_cycle(const vec<vec<int>> &g) {
     }
   }
 
-  vec<bool> removed(cnt);
-  vec<int> stk, res;
-  stk.pb(0);
+  vector<bool> removed(cnt);
+  vector<int> stk, res;
+  stk.push_back(0);
   while (sz(stk)) {
     int u = stk.back();
     if (deg[u] == 0) {
-      res.pb(u);
+      res.push_back(u);
       stk.pop_back();
     } else {
       while (removed[h[u].back().se]) {
@@ -38,7 +38,7 @@ vec<int> euler_cycle(const vec<vec<int>> &g) {
       deg[u]--, deg[v]--;
       removed[idx] = true;
 
-      stk.pb(v);
+      stk.push_back(v);
     }
   }
   return res;
