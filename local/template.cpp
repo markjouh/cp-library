e76f48066123d610b651a9cc4864a0cad3d25e99
Mark Zhou <xinyunzhouu@gmail.com>
2024-09-05 20:16:10 -0400

    Add bits header, edit template

--- local/template.cpp
+++ local/template.cpp
@@ -1,21 +1,4 @@
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <cstdint>
-#include <cstring>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -25,16 +8,6 @@ using namespace std;
 #define dbg(...)
 #endif
 
-#define arg4(a, b, c, d, ...) d
- 
-#define rep3(i, l, r) for (int i = int(l); i < int(r); i++)
-#define rep2(i, n) rep3(i, 0, n)
-#define rep(...) arg4(__VA_ARGS__, rep3, rep2) (__VA_ARGS__)
- 
-#define per3(i, l, r) for (int i = int(r) - 1; i >= int(l); i--)
-#define per2(i, n) per3(i, 0, n)
-#define per(...) arg4(__VA_ARGS__, per3, per2) (__VA_ARGS__)
-
 #define all(x) begin(x), end(x)
 #define sz(x) int(size(x))
 #define pb push_back
@@ -43,8 +16,6 @@ using namespace std;
 #define se second
 
 using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
 
 template <class T>
 bool ckmin(T &a, const T &b) {
