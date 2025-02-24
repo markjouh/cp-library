cdbbbf435e619ab5dfd9e72be0a1f7de77d31338
Mark Zhou <github.chaps279@passmail.net>
2025-02-24 12:15:36 -0500

    Update ordered_set.h

--- datastructures/ordered_set.h
+++ datastructures/ordered_set.h
@@ -8,23 +8,25 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 template <class T>
 struct ordered_multiset {
-    int time = 0;
-    ordered_set<pair<T, int>> st;
-
-    void insert(T x) {
-        st.insert({x, time++});
+  int time = 0;
+  ordered_set<pair<T, int>> st;
+ 
+  void insert(T x) {
+    st.insert({x, time++});
+  }
+ 
+  void extract(T x) {
+    auto it = st.lower_bound({x, 0});
+    if (it != end(st) && it->first == x) {
+      st.erase(it);
     }
-    void extract(T x) {
-        auto it = st.lower_bound({x, 0});
-        if (it != end(st) && it->fi == x) {
-            st.erase(it);
-        }
-    }
-
-    T find_by_order(int k) {
-        return (*st.find_by_order(k)).first;
-    }
-    int order_of_key(T x) {
-        return st.order_of_key({x, 0});
-    }
+  }
+ 
+  T find_by_order(int k) {
+    return (*st.find_by_order(k)).first;
+  }
+ 
+  int order_of_key(T x) {
+    return st.order_of_key({x, 0});
+  }
 };
